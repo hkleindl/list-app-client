391a95c563bf65e148af9e91621ab5a5307a309a
hkleindl <hkleindl@gmail.com>
2020-05-28 17:04:46 -0400

    Add login form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Login from "./components/Login.js"
 
 class App extends Component {
   render() {
     return (
-      "React List App!"
-    );
+      <Login/>
+    )
   }
   
 }
---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Login = () => {
+    return (
+        <form onSubmit={}>
+            <input type="text" name="username" value={} onChange={} placeholder="Username"/>
+            <input type="password" name="password" value={} onChange={} placeholder="Password"/>
+            <input type="submit" value="Log In"/>
+        </form>
+    )
+}
+
+export default Login
