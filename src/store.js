3ef7487e078868f2047a7aebf4f4506c701bf5c7
hkleindl <hkleindl@gmail.com>
2020-05-28 16:52:31 -0400

    Add login form reducer

--- src/store.js
+++ src/store.js
@@ -2,10 +2,12 @@ import thunk from 'redux-thunk'
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 import users from "./reducers/users.js"
 import currentUser from "./reducers/currentUser.js"
+import loginForm from "./reducers/loginForm.js"
 
 const reducer = combineReducers({
   users,  
-  currentUser
+  currentUser,
+  loginForm
   })
   
   const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
