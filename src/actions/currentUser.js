2fea96b83b0b595f8e9037784adc8fe59fb85add
hkleindl <hkleindl@gmail.com>
2020-05-29 17:32:54 -0400

    add get current user action creator

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -28,3 +28,25 @@ export const login = credentials => {
             .catch(console.log)
     }
 }
+
+export const getCurrentUser = credentials => {
+    return dispatch => {
+        const configObj = {
+            method: "GET",
+            headers: {
+                "Content-Type" : "application/json"
+            }
+        }
+        return fetch("http://localhost:3000/api/v1/login", configObj)
+            .then(r => r.json())
+            .then(user => {
+                if (user.error) {
+                    alert(user.error)
+                } else {
+                    dispatch(setCurrentUser(user))
+                }
+            }  
+            )
+            .catch(console.log)
+    }
+}
