2a292c17ecc7f0e80700ac1735ffe29fbcbf9afa
hkleindl <hkleindl@gmail.com>
2020-06-11 16:30:17 -0400

    add navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,39 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import Login from "./Login.js"
-import Logout from "./Logout.js"
+import UserMenu from "./UserMenu.js"
 
 const Navbar = ({ currentUser }) => {
     
     return (
-        <div className="Navbar">
-            { currentUser ? `Welcome, ${currentUser.attributes.username}` : <p>Welcome to List App</p>}
-            { currentUser ? <Logout/> : <Login/> }
+        <div>
+            <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+                <a class="navbar-brand" href="#">Expand at md</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarsExample04">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="#">Disabled</a>
+                        </li>
+                    </ul>
+
+                    <ul class="navbar-nav ml-auto">
+                        { currentUser ?
+                            <UserMenu /> :
+                            <Login />
+                        }
+                    </ul>
+                </div>
+            </nav>
         </div>
     )
 }
