3f34e7321fb66947091570358b27ba4ade6b8f1c
hkleindl <hkleindl@gmail.com>
2020-06-12 14:29:43 -0400

    add nav links component

---
+++ src/components/NavLinks.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const NavLinks = () => {
+    
+    return (
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#">Disabled</a>
+            </li>
+        </ul>
+    )
+}
+
+export default NavLinks;
