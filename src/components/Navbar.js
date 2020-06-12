0a2dd93faf800ea03b7e72ca778792d57d309705
hkleindl <hkleindl@gmail.com>
2020-06-12 14:28:53 -0400

    only render nav links if user is logged in

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,29 +2,24 @@ import React from 'react'
 import { connect } from 'react-redux'
 import Login from "./Login.js"
 import UserMenu from "./UserMenu.js"
+import NavLinks from "./NavLinks.js"
 
 const Navbar = ({ currentUser }) => {
     
     return (
         <div>
             <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-                <a class="navbar-brand" href="#">Expand at md</a>
+                <a class="navbar-brand" href="#">List App</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-
+                
                 <div class="collapse navbar-collapse" id="navbarsExample04">
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link disabled" href="#">Disabled</a>
-                        </li>
-                    </ul>
+                    
+                    { currentUser ?
+                        <NavLinks /> :
+                        ""
+                    }
 
                     <ul class="navbar-nav ml-auto">
                         { currentUser ?
@@ -32,6 +27,7 @@ const Navbar = ({ currentUser }) => {
                             <Login />
                         }
                     </ul>
+                    
                 </div>
             </nav>
         </div>
