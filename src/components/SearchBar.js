13dd14ea47fc4fb9488380ed745de3fb5fefdd08
hkleindl <hkleindl@gmail.com>
2020-06-23 16:40:13 -0400

    add styling

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,7 +3,7 @@ import React from 'react'
 const SearchBar = () => {
     
     return (
-        <div class="input-group input-group-sm">
+        <div class="input-group input-group-sm mt-1 ml-2">
             <input type="text" class="form-control" placeholder="Search" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
             <div class="input-group-append input-group-sm">
                 <button class="btn btn-secondary form-control" type="button">
