0df25f4bf69b5b39c606834fb3f0360db407c8b7
hkleindl <hkleindl@gmail.com>
2020-08-22 22:15:17 -0400

    add update and clear sign up form action cases

--- src/reducers/signUpForm.js
+++ src/reducers/signUpForm.js
@@ -5,6 +5,10 @@ const initialState = {
 
 export default (initialState, action) => {
     switch (action.type) {
+        case 'UPDATE_SIGN_UP_FORM':
+            return action.formData
+        case 'CLEAR_SIGN_UP_FORM':
+            return initialState
         default:
             return state
     }
