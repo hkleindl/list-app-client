49e4229f17c038875df901be0e5d0f654507580f
hkleindl <hkleindl@gmail.com>
2020-10-08 17:00:41 -0400

    import BrowserRouter

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux'
 import store from './store.js'
+import { BrowserRouter as Router} from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={ store }>
-      <App />
-    </Provider>
+    <Router>
+      <Provider store={ store }>
+        <App />
+      </Provider>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
