6626bc810ea22d5f1a95df691fdd60193b39c88c
hkleindl <hkleindl@gmail.com>
2020-10-08 17:04:32 -0400

    dynamically render elements

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,29 +3,41 @@ import { connect } from 'react-redux'
 import Login from "./Login.js"
 import UserMenu from "./UserMenu.js"
 import NavLinks from "./NavLinks.js"
+import { Link } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 
 const Navbar = ({ currentUser }) => {
-    
+    let location = useLocation()
+    console.log(location.pathname)
+
+    let navBarContent = () => {
+        if (currentUser) {
+            return <UserMenu />
+        } else if (location.pathname === '/signup') {
+            return null
+        } else {
+            return <Login />
+        }
+    }
+
     return (
         <div>
-            <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-                <a class="navbar-brand" href="#">List App</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+                <Link to="/" className="navbar-brand">List App</Link>
+                {/* <a class="navbar-brand" href="#">List App</a> */}
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
                 
-                <div class="collapse navbar-collapse" id="navbarsExample04">
+                <div className="collapse navbar-collapse" id="navbarsExample04">
                     
                     { currentUser ? 
                             <NavLinks /> :       
                             null
                     }
 
-                    <ul class="navbar-nav ml-auto">
-                        { currentUser ?
-                            <UserMenu /> :
-                            <Login />
-                        }
+                    <ul className="navbar-nav ml-auto">
+                        {navBarContent()}
                     </ul>
 
                 </div>
